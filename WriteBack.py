3bb146681446e34fc679d3fe65fa85bb1d5cff53
fiq362 <austincarpenter0815@gmail.com>
2018-11-09 16:42:39 -0600

    Added self

--- WriteBack.py
+++ WriteBack.py
@@ -3,7 +3,7 @@ class WriteBack:
     def __init__(self):
         self.cache = None
 
-    def write(address):
+    def write(self, address):
         tag, set_id, offset = address
         for cache_line in cache.sets[set_id].ways:
             if tag == cache_line["tag"]:
@@ -13,5 +13,5 @@ class WriteBack:
         cache.read(address)
 
 
-    def upon_eviction(address):
+    def upon_eviction(self, address):
         print("Writing to memory")
--- WriteThrough.py
+++ WriteThrough.py
@@ -3,11 +3,11 @@ class WriteThrough:
     def __init__(self):
         self.cache = None
 
-    def write(address):
+    def write(self, address):
         tag, set_id, offset = address
         for cache_line in cache.sets[set_id].ways:
             if tag == cache_line["tag"]:
                 cache_line["valid"] = 0 
 
-    def upon_eviction(address):
+    def upon_eviction(self, address):
         print("Write to memory")   
