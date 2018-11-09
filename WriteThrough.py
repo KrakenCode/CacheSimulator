681f7ea7e9688493feb144da9ec8f0af2cd4aff2
fiq362 <austincarpenter0815@gmail.com>
2018-11-09 17:15:31 -0600

    Self thing

--- WriteThrough.py
+++ WriteThrough.py
@@ -5,7 +5,7 @@ class WriteThrough:
 
     def write(self, address):
         tag, set_id, offset = address
-        for cache_line in cache.sets[set_id].ways:
+        for cache_line in self.cache.sets[set_id].ways:
             if tag == cache_line["tag"]:
                 cache_line["valid"] = 0 
 
